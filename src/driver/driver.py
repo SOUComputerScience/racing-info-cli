d0061e0252d43e91bce850e15fba1c5741afdbca
Sam Platt <platts1@sou.edu>
2023-06-05 16:17:04 -0700

    add support for suffixes

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -4,7 +4,7 @@ class Driver():
     """
     a class to represent a driver
     """
-    def __init__(self, surname: str, givenname: str, number: int, nationality: str, surname_first: bool = False):
+    def __init__(self, surname: str, givenname: str, number: int, nationality: str, surname_first: bool = False, suffix: str = None):
         
         # type checking
 
@@ -18,12 +18,15 @@ class Driver():
             raise TypeError(f"{nationality} must be of type str, not {type(nationality)}")
         if not isinstance(surname_first, bool):
             raise TypeError(f"{surname_first} must be of type bool, not {type(surname_first)}")
-        
+        if not isinstance(suffix, (str, type(None))):
+            raise TypeError(f"{suffix} must of type str or None, not {type(suffix)}")
+
         self.surname = surname
         self.givenname = givenname
         self.number = number
         self.nationality = nationality
         self.surname_first = surname_first
+        self.suffix = suffix
 
     def __str__(self) -> str:
         '''
@@ -32,7 +35,18 @@ class Driver():
         returns:
         str
         '''
+        s = str()
+
+        # add driver first and last names according to driver's preference in order
+
         if self.surname_first:
-            return f"{self.surname.upper()} {self.givenname} #{self.number}"
-        
-        return f"{self.givenname} {self.surname.upper()} #{self.number}"
+            s += f"{self.surname.upper()} {self.givenname}"
+        else:
+            s += f"{self.givenname} {self.surname.upper()}"
+
+        if self.suffix: # add suffix if applicable
+            s += f" {self.suffix}"
+
+        s += f" #{self.number}" # add driver number
+
+        return s
