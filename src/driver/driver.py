ff8539cf87ed5ab5fce421f7cf0eda0f0c237bd6
Sam Platt <platts1@sou.edu>
2023-06-04 23:42:44 -0700

    add attribute for if driver's surname should be before the givenname, and associated method for formatting the name

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -1,7 +1,20 @@
 
 
 class Driver():
-    def __init__(self, surname: str, givenname: str, nationality: str):
+    def __init__(self, surname: str, givenname: str, nationality: str, surname_first: bool = False):
         self.surname = surname
         self.givenname = givenname
         self.nationality = nationality
+        self.surname_first = surname_first
+
+    def getNameFormatted(self) -> str:
+        '''
+        formats the name of the driver as `LAST First` or `First LAST` depending on driver preference
+        
+        returns:
+        str
+        '''
+        if self.surname_first:
+            return f"{self.surname.upper()} {self.givenname}"
+        
+        return f"{self.givenname} {self.surname.upper()}"
