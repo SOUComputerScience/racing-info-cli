111e189a8928ecdb57cdf19861a9830bee8aa448
Sam Platt <platts1@sou.edu>
2023-06-05 15:49:16 -0700

    add error checking and driver number

--- src/driver/driver.py
+++ src/driver/driver.py
@@ -1,9 +1,26 @@
 
 
 class Driver():
-    def __init__(self, surname: str, givenname: str, nationality: str, surname_first: bool = False):
+    """
+    TODO
+    """
+    def __init__(self, surname: str, givenname: str, number: int, nationality: str, surname_first: bool = False):
+        # type checking
+
+        if not isinstance(surname, str):
+            raise TypeError(f"") # TODO
+        if not isinstance(givenname, str):
+            raise TypeError(f"") # TODO
+        if not isinstance(number, int):
+            raise TypeError(f"") # TODO
+        if not isinstance(nationality, str):
+            raise TypeError(f"") # TODO
+        if not isinstance(surname_first, bool):
+            raise TypeError(f"") # TODO
+        
         self.surname = surname
         self.givenname = givenname
+        self.number = number
         self.nationality = nationality
         self.surname_first = surname_first
 
@@ -15,6 +32,6 @@ class Driver():
         str
         '''
         if self.surname_first:
-            return f"{self.surname.upper()} {self.givenname}"
+            return f"{self.surname.upper()} {self.givenname} #{self.number}"
         
-        return f"{self.givenname} {self.surname.upper()}"
+        return f"{self.givenname} {self.surname.upper()} #{self.number}"
