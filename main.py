c641d0da9027c87caf266fd0db8fd58b9b11bb5d
Sam Platt <platts1@sou.edu>
2023-06-07 14:41:17 -0700

    added drivers, adding races

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.championship.championship import Championship
 from src.driver.driver import Driver
 from src.engine.engine import *
 from src.race.race import Race
+from src.race.raceResults import RaceResults
 from src.series.series import Series
 from src.team.team import Team
 from src.team.teamFactory import TeamFactory
@@ -14,103 +15,122 @@ def main():
     # instantiate the 2023 Formula One Championship
     formula_one_2023 = Championship(series = formula_one, year = 2023)
 
+    # instantiate drivers
+
+    alb = Driver(surname = "Albon", givenname = "Alexander", number = 23, nationality = "Thailand")
+    alo = Driver(surname = "Alonso", givenname = "Fernando", number = 14, nationality = "Spain")
+    bot = Driver(surname = "Bottas", givenname = "Valtteri", number = 77, nationality = "Finland")
+    dev = Driver(surname = "de Vries", givenname = "Nyck", number = 21, nationality = "Netherlands")
+    gas = Driver(surname = "Gasly", givenname = "Pierre", number = 10, nationality = "France")
+    ham = Driver(surname = "Hamilton", givenname = "Lewis", number = 44, nationality = "United Kingdom")
+    hul = Driver(surname = "Hülkenberg", givenname = "Nico", number = 27, nationality = "Germany")
+    lec = Driver(surname = "Leclerc", givenname = "Charles", number = 16, nationality = "Monaco")
+    mag = Driver(surname = "Magnussen", givenname = "Kevin", number = 20, nationality = "Denmark")
+    nor = Driver(surname = "Norris", givenname = "Lando", number = 4, nationality = "United Kingdom")
+    oco = Driver(surname = "Ocon", givenname = "Esteban", number = 31, nationality = "France")
+    per = Driver(surname = "Pérez", givenname = "Sergio", number = 11, nationality = "Mexico")
+    pia = Driver(surname = "Piastri", givenname = "Oscar", number = 81, nationality = "Australia")
+    rus = Driver(surname = "Russell", givenname = "George", number = 63, nationality = "United Kingdom")
+    sai = Driver(surname = "Sainz", suffix = "Jr.", givenname = "Carlos", number = 55, nationality = "Spain")
+    sar = Driver(surname = "Sargeant", givenname = "Logan", number = 2, nationality = "United States")
+    stroll = Driver(surname = "Stroll", givenname = "Lance", number = 18, nationality = "Canada") # str is a reserved word
+    tsu = Driver(surname = "Tsunoda", givenname = "Yuki", number = 22, nationality = "Japan")
+    ver = Driver(surname = "Verstappen", givenname = "Max", number = 1, nationality = "Netherlands")
+    zho = Driver(surname = "Zhou", givenname = "Guanyu", number = 24, nationality = "China", surname_first = True)
+
     # create teams using TeamFactory
 
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamFerrariEngine(
             name = "Alfa Romeo",
             country = "Switzerland",
-            drivers = [
-                Driver(surname = "Bottas", givenname = "Valtteri", number = 77, nationality = "Finland"),
-                Driver(surname = "Zhou", givenname = "Guanyu", number = 24, nationality = "China", surname_first = True)
-            ]
+            drivers = [bot, zho]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamHondaRBPTEngine(
             name = "AlphaTauri",
             country = "Italy",
-            drivers = [
-                Driver(surname = "de Vries", givenname = "Nyck", number = 21, nationality = "Netherlands"),
-                Driver(surname = "Tsunoda", givenname = "Yuki", number = 22, nationality = "Japan")
-            ]
+            drivers = [dev, tsu]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamRenaultEngine(
             name = "Alpine",
             country = "France",
-            drivers = [
-                Driver(surname = "Gasly", givenname = "Pierre", number = 10, nationality = "France"),
-                Driver(surname = "Ocon", givenname = "Esteban", number = 31, nationality = "France")
-            ]
+            drivers = [gas, oco]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamMercedesEngine(
             name = "Aston Martin",
             country = "United Kingdom",
-            drivers = [
-               Driver(surname = "Alonso", givenname = "Fernando", number = 14, nationality = "Spain"),
-               Driver(surname = "Stroll", givenname = "Lance", number = 18, nationality = "Canada")
-            ]
+            drivers = [alo, stroll]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamFerrariEngine(
             name = "Ferrari", # this is my second favorite team. They are terrible at strategy and there are multiple memes about how bad their strategy is
             country = "Italy",
-            drivers = [
-                Driver(surname = "Leclerc", givenname = "Charles", number = 16, nationality = "Monaco"),
-                Driver(surname = "Sainz", suffix = "Jr.", givenname = "Carlos", number = 55, nationality = "Spain")
-            ]
+            drivers = [lec, sai]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamFerrariEngine(
             name = "Haas",
             country = "United States",
-            drivers = [
-                # TODO add drivers
-            ]
+            drivers = [hul, mag]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamMercedesEngine(
             name = "McLaren",
             country = "United Kingdom",
-            drivers = [
-                Driver(surname = "Norris", givenname = "Lando", number = 4, nationality = "United Kingdom"),
-                Driver(surname = "Piastri", givenname = "Oscar", number = 81, nationality = "Australia")
-            ]
+            drivers = [nor, pia]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamMercedesEngine(
             name = "Mercedes", # this is my favorite team
             country = "Germany",
-            drivers = [
-                Driver(surname = "Hamilton", givenname = "Lewis", number = 44, nationality = "United Kingdom"),
-                Driver(surname = "Russell", givenname = "George", number = 63, nationality = "United Kingdom")
-            ]
+            drivers = [ham, rus]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamHondaRBPTEngine(
             name = "Red Bull", # you're not going to believe this, but Red Bull is actually a better team right now than Ferrari
             country = "Austria",
-            drivers = [
-                # TODO add drivers
-            ]
+            drivers = [per, ver]
         )
     )
     formula_one_2023.addTeamToChampionship(
         TeamFactory.createTeamMercedesEngine(
             name = "Williams",
             country = "United Kingdom",
-            drivers = [
-                # TODO add drivers
-            ]
+            drivers = [alb, sar]
+        )
+    )
+
+    # create races
+
+    bahrain_gp = Race(
+        name = "Bahrain Grand Prix",
+        country = "Bahrain",
+        city = "Sakhir",
+        circuit = "Bahrain International Circuit"
+    ),
+
+    # add race results
+
+    print(bahrain_gp.__str__())
+
+    formula_one_2023.holdRace(
+        RaceResults(
+            race = bahrain_gp,
+            rankings = list(
+                
+            ), # TODO
+            fastestLap = Driver() # TODO
         )
     )
 
