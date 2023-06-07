9add6e3b8006ea8da893c6ffff4b35d0a8268d86
Sam Platt <platts1@sou.edu>
2023-06-07 16:02:35 -0700

    unit tests for driver

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,4 +1,44 @@
 import pytest
+from src.driver.driver import Driver
 from typing import Any
 
-# TODO
+@pytest.fixture
+def driver_fixture():
+    return Driver(
+        surname = "Doe",
+        givenname = "John",
+        number = 0,
+        nationality = "Internet",
+        surname_first = False,
+        suffix = "Jr."
+    )
+
+@pytest.fixture
+def driver_fixture():
+    return Driver(
+        surname = "Doe",
+        givenname = "John",
+        number = 0,
+        nationality = "Internet",
+        surname_first = False,
+        suffix = "Jr."
+    )
+
+def test___str___(driver_fixture: Driver):
+    d = driver_fixture
+    s = d.__str__()
+
+    assert isinstance(d, Driver)
+    assert isinstance(s, str)
+    
+    assert s == "John DOE Jr. #0"
+
+def test___str___last_first(driver_fixture: Driver):
+    d = driver_fixture
+    d.surname_first = True
+    s = d.__str__()
+
+    assert isinstance(d, Driver)
+    assert isinstance(s, str)
+
+    assert s == "DOE John Jr. #0"
