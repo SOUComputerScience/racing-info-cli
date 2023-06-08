edcdff8b0eea50e6aee550146b380e23767a09d0
Sam Platt <platts1@sou.edu>
2023-06-08 14:32:29 -0700

    fixtures and imports

--- tests/test_championship.py
+++ tests/test_championship.py
@@ -1,9 +1,18 @@
+from copy import deepcopy
 import pytest
+from src.championship.championship import Championship
 from src.driver.driver import Driver
 from src.race.race import Race
 from src.race.raceResults import RaceResults
 from src.series.series import Series
 from src.team.team import Team
+from tests.dummy_data import * # TODO edit this later to only import used dummy vars 
 from typing import Any, List
 
-# TODO
+# fixtures
+
+@pytest.fixture
+def championship_fixture():
+    return deepcopy(DUMMY_CHAMPIONSHIP)
+
+# TODO test cases
