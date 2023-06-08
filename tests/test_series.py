54ba35f250a3ab2729a80c52763fc397874a6090
Sam Platt <platts1@sou.edu>
2023-06-08 14:24:51 -0700

    series class test cases

--- tests/test_series.py
+++ tests/test_series.py
@@ -7,3 +7,11 @@ from typing import Any
 @pytest.fixture
 def series_fixture():
     return deepcopy(DUMMY_SERIES)
+
+def test___str__(series_fixture: Series):
+    assert isinstance(series_fixture, Series)
+    
+    s = series_fixture.__str__()
+    
+    assert isinstance(s, str)
+    assert s == "Dummy Series"
