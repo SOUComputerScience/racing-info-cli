83e26ae276adb3ad179f6bc72b27a4c1a5afc04c
Sam Platt <platts1@sou.edu>
2023-06-09 11:50:35 -0700

    unit tests

--- tests/test_race.py
+++ tests/test_race.py
@@ -1,3 +1,16 @@
+from copy import deepcopy
 import pytest
 from src.race.race import Race
-from typing import Any
+from tests.dummy_data import DUMMY_RACE
+
+@pytest.fixture
+def race_fixture():
+    return deepcopy(DUMMY_RACE)
+
+def test___str__(race_fixture: Race):
+    assert isinstance(race_fixture, Race)
+    
+    s = race_fixture.__str__()
+    
+    assert isinstance(s, str)
+    assert s == f"Dummy Race (Internet Circuit in Internet City, Internet)"
